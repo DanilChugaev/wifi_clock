491dd6464dce685e50af044d25570a87dacffb3a
danilchugaev <dmchugaev@gmail.com>
2021-04-24 23:33:28 +0700

    Добавляет подключение к wi-fi

--- src/wifi_clock/wifi_clock.ino
+++ src/wifi_clock/wifi_clock.ino
@@ -18,20 +18,29 @@
 #define NTP_INTERVAL 60 * 60 * 1000   // обновление времени с сервера (1 час)
 #define DISPLAY_INTERVAL 1000              // обновление времени на дисплее (1 секунда)
 
+// -------- Менеджер WiFi ---------
+#define AC_SSID "WiFi_Clock"
+#define AC_PASS "time_to_sleep"
+
 // ---------------- БИБЛИОТЕКИ -----------------
-#include "microDS3231.h" //https://alexgyver.ru/microlibs/ + https://github.com/AlexGyver/GyverLibs/tree/master/microDS3231
+#include "microDS3231.h" // https://alexgyver.ru/microlibs/ + https://github.com/AlexGyver/GyverLibs/tree/master/microDS3231
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager/tree/0.16.0
 
 // ------------------- ТИПЫ --------------------
 MicroDS3231 rtc;
+WiFiManager wifiManager;
 
 // ----------------- ПЕРЕМЕННЫЕ ----------------
 boolean internet_connected = false;
+String clock_ip = "";
 
 void setup() {
   Serial.begin(115200);
+
+  connectToWiFi();
 }
 
 void loop() {
-  printTime();
+//  printTime();
   delay(1000);
 }
